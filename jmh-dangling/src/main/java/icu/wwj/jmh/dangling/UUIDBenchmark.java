ee2dc561ef72e8f23b51d1bd6814b4cb763e8a6e
吴伟杰 <wuweijie@apache.org>
2021-08-24 17:27:50 +0800

    Update UUIDBenchmark

--- jmh-dangling/src/main/java/icu/wwj/jmh/dangling/UUIDBenchmark.java
+++ jmh-dangling/src/main/java/icu/wwj/jmh/dangling/UUIDBenchmark.java
@@ -2,21 +2,42 @@ package icu.wwj.jmh.dangling;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
 
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 
 @Fork(1)
-@Measurement(iterations = 5, time = 3)
-@Warmup(iterations = 5, time = 3)
-@State(Scope.Benchmark)
 public class UUIDBenchmark {
     
     @Benchmark
     public UUID randomUUID() {
         return UUID.randomUUID();
+    }
+    
+    @Benchmark
+    public String randomUUIDToString() {
+        return UUID.randomUUID().toString();
+    }
+    
+    @Benchmark
+    public UUID threadLocalRandomUUID() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        return new UUID(random.nextLong(), random.nextLong());
+    }
+    
+    @Benchmark
+    public String threadLocalRandomUUIDToString() {
+        ThreadLocalRandom random = ThreadLocalRandom.current();
+        return new UUID(random.nextLong(), random.nextLong()).toString();
+    }
+    
+    @Benchmark
+    public UUID threadLocalRandomEachTimeUUID() {
+        return new UUID(ThreadLocalRandom.current().nextLong(), ThreadLocalRandom.current().nextLong());
+    }
+    
+    @Benchmark
+    public String threadLocalRandomEachTimeUUIDToString() {
+        return new UUID(ThreadLocalRandom.current().nextLong(), ThreadLocalRandom.current().nextLong()).toString();
     }
 }
