0f1d5ca0abcd85545ebebc10b5a337f46d088cce
吴伟杰 <wuweijie@apache.org>
2021-09-01 13:34:26 +0800

    Add absolute path configurations

--- jmh-jdbc/src/main/java/icu/wwj/jmh/jdbc/Jdbcs.java
+++ jmh-jdbc/src/main/java/icu/wwj/jmh/jdbc/Jdbcs.java
@@ -1,5 +1,8 @@
 package icu.wwj.jmh.jdbc;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.util.Properties;
@@ -11,19 +14,23 @@ public final class Jdbcs {
     }
     
     public static Connection getConnection() {
-        return getConnection(System.getProperty("profile"));
+        InputStream in = null;
+        String profile = System.getProperty("profile");
+        String configurationFile = System.getProperty("conf");
+        try {
+            in = null == profile || profile.trim().isEmpty() ? new FileInputStream(configurationFile) : Jdbcs.class.getResourceAsStream(String.format("/META-INF/profiles/%s.properties", profile));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(String.format("Fail to get profile %s or conf %s", profile, configurationFile), e);
+        }
+        return getConnection(in);
     }
     
-    public static Connection getConnection(final String profile) {
-        if (null == profile || profile.trim().isEmpty()) {
-            throw new RuntimeException("Please specify -Dprofile");
-        }
+    public static Connection getConnection(final InputStream in) {
         Properties props = new Properties();
-        String path = String.format("/META-INF/profiles/%s.properties", profile);
         try {
-            props.load(Jdbcs.class.getResourceAsStream(path));
+            props.load(in);
         } catch (Exception e) {
-            throw new RuntimeException(String.format("Fail to get %s", path), e);
+            throw new RuntimeException(e);
         }
         try {
             return DriverManager.getConnection(props.getProperty("jdbc-url"), props.getProperty("username"), props.getProperty("password"));
