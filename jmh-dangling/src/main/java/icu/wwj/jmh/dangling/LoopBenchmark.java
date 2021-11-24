6db947f55bffeae5c9195c59e5c258eadd7ac588
吴伟杰 <wuweijie@apache.org>
2021-11-24 13:28:31 +0800

    Bench loop

---
+++ jmh-dangling/src/main/java/icu/wwj/jmh/dangling/LoopBenchmark.java
@@ -0,0 +1,46 @@
+package icu.wwj.jmh.dangling;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+@State(Scope.Benchmark)
+public class LoopBenchmark {
+    
+    private final List<Integer> list = new ArrayList<>(); 
+    
+    @Setup
+    public void setup() {
+        for (int i = 0; i < 16; i++) {
+            list.add(ThreadLocalRandom.current().nextInt());
+        }
+    }
+    
+    @Benchmark
+    public int benchForIndex() {
+        int sum = 0;
+        for (int i = 0; i < list.size(); i++) {
+            sum += list.get(i);
+        }
+        return sum;
+    }
+    
+    @Benchmark
+    public int benchForEach() {
+        int sum = 0;
+        for (Integer each : list) {
+            sum += each;
+        }
+        return sum;
+    }
+    
+    @Benchmark
+    public int benchStream() {
+        return list.stream().mapToInt(each -> each).sum();
+    }
+}
