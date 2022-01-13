601512abd186c0eb008b34dec0985ae0d4db66fe
吴伟杰 <wuweijie@apache.org>
2022-01-13 13:37:29 +0800

    Update benchmarks for ShardingSphere3

--- jmh-shardingsphere3/src/main/java/icu/wwj/jmh/shardingsphere3/ReadWriteBenchmarkShardingSphere3.java
+++ jmh-shardingsphere3/src/main/java/icu/wwj/jmh/shardingsphere3/ReadWriteBenchmarkShardingSphere3.java
@@ -1,12 +1,12 @@
 package icu.wwj.jmh.shardingsphere3;
 
-import icu.wwj.jmh.jdbc.UnpooledPointSelectBenchmarkBase;
+import icu.wwj.jmh.jdbc.UnpooledReadWriteBenchmarkBase;
 import lombok.SneakyThrows;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 
-public class PointSelectBenchmarkShardingSphere3 extends UnpooledPointSelectBenchmarkBase {
+public class ReadWriteBenchmarkShardingSphere3 extends UnpooledReadWriteBenchmarkBase {
     
     private static final DataSource DATA_SOURCE = ShardingSpheres.createDataSource("/sharding-jdbc/sysbench-remote-single.yaml");
     
