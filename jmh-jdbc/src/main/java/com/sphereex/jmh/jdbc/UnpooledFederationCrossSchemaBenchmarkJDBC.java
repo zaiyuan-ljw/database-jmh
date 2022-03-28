64759a69f445416f4e87646f9045c9534c92802f
Nianjun Sun <sunnianjun@sphere-ex.com>
2022-03-28 13:22:07 +0800

    Add : add Federation test case

---
+++ jmh-jdbc/src/main/java/com/sphereex/jmh/jdbc/UnpooledFederationCrossSchemaBenchmarkJDBC.java
@@ -0,0 +1,6 @@
+package com.sphereex.jmh.jdbc;
+
+import com.sphereex.jmh.jdbc.connection.UnpooledJDBCConnectionProvider;
+
+public class UnpooledFederationCrossSchemaBenchmarkJDBC extends UnpooledFederationCrossSchemaBenchmarkBase implements UnpooledJDBCConnectionProvider {
+}
---
+++ jmh-jdbc/src/main/java/com/sphereex/jmh/jdbc/UnpooledFederationSameSchemaBenchmarkJDBC.java
@@ -0,0 +1,6 @@
+package com.sphereex.jmh.jdbc;
+
+import com.sphereex.jmh.jdbc.connection.UnpooledJDBCConnectionProvider;
+
+public class UnpooledFederationSameSchemaBenchmarkJDBC extends UnpooledFederationSameSchemaBenchmarkBase implements UnpooledJDBCConnectionProvider {
+}
